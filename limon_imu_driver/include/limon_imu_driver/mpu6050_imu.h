dab0a9ec73a8da5756d0d2c6e6705027414f2e33
wangzheqie <wangzheqie@qq.com>
2021-07-17 17:30:48 +0800

    add mpu6050 driver

--- limon_imu_driver/include/limon_imu_driver/mpu6050_imu.h
+++ limon_imu_driver/include/limon_imu_driver/mpu6050_imu.h
@@ -3,7 +3,18 @@
 
 using namespace westonrobot;
 
+#define MPU6050_FRAME_LEN 11
+#define TTL_FRAME_START 0x55
+
 namespace agx {
+
+enum TTLFrameType {
+  TIME = 0x50,
+  ACCELERATION = 0x51,
+  ANGULAR_VELOCITY = 0x52,
+  ANGLE = 0x53,
+};
+
 class SerialRead {
  public:
   SerialRead();
@@ -12,8 +23,13 @@ class SerialRead {
   void Connect(std::string dev_name, uint32_t bouad_rate,
                SerialFrameCallback cb);
   void Disconnect();
-  void SendData(uint8_t *data, size_t len);
+  void SendData(uint8_t* data, size_t len);
   void ReadRawSerialData(uint8_t* data, const size_t bufsize, size_t len);
+  int ParseRawData(const std::vector<uint8_t>& data);
+  void ParseSingleFrame(const std::vector<uint8_t>& data, TTLFrameType type);
+  void ParseAcceleration(const std::vector<uint8_t>& data);
+  void ParseAngularVelocity(const std::vector<uint8_t>& data);
+  void ParseAngle(const std::vector<uint8_t>& data);
 
  private:
   std::shared_ptr<AsyncSerial> serial_;
@@ -23,6 +39,6 @@ class SerialRead {
   uint32_t timeout_ms_{500};
   uint32_t watchdog_counter_{0};
   std::vector<uint8_t> binarystream_;
-  char *bytestream_{nullptr};
+  char* bytestream_{nullptr};
 };
 };  // namespace agx
--- limon_imu_driver/src/mpu6050_imu.cpp
+++ limon_imu_driver/src/mpu6050_imu.cpp
@@ -1,7 +1,10 @@
 
 
 #include "limon_imu_driver/mpu6050_imu.h"
+#include <memory.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace agx;
 #define MAX_BUFF_SIZE 10240
@@ -9,8 +12,6 @@ using namespace agx;
 SerialRead::SerialRead() {
   bytestream_ = new char[MAX_BUFF_SIZE];
   memset(bytestream_, 0, MAX_BUFF_SIZE);
-
-  binarystream_.resize(MAX_BUFF_SIZE);
 }
 
 SerialRead::~SerialRead() {
@@ -44,14 +45,129 @@ void SerialRead::Connect(std::string dev_name, uint32_t bouad_rate,
 void SerialRead::ReadRawSerialData(uint8_t* data, const size_t bufsize,
                                    size_t len) {
   std::lock_guard<std::mutex> lock(state_mutex_);
-  if (len == 0) {
+
+  // max read data number per loop
+  if (len > MAX_BUFF_SIZE) {
+    len = MAX_BUFF_SIZE;
+  } else if (len == 0) {
     return;
-  } else {
-    // copy the data from callback stack
-    memcpy(&bytestream_[0], (char*)data, len);
   }
 
-  printf("get the data: %d\n", len);
+  // clear the older data
+  size_t curr_len = binarystream_.size();
+  if (curr_len >= MAX_BUFF_SIZE - len) {
+    binarystream_.clear();
+    curr_len = 0;
+  }
+
+  // save the raw data to a vector
+  for (int i = 0; i < len; i++) {
+    binarystream_.push_back(*(&data[0] + i));
+  }
+  curr_len = binarystream_.size();
+
+  // parse the data from buff vector
+  int use_len = ParseRawData(binarystream_);
+
+  // if raw data have bean used with `use_len`, remove use_len datas at the
+  // vector front
+  if (use_len > 0) {
+    binarystream_.erase(binarystream_.begin(), binarystream_.begin() + use_len);
+  }
+}
+
+int SerialRead::ParseRawData(const std::vector<uint8_t>& data) {
+  int use_len = 0;
+  if (data.size() < MPU6050_FRAME_LEN) {
+    return 0;
+  }
+  if (data[0] == TTL_FRAME_START) {
+    ParseSingleFrame(data, static_cast<TTLFrameType>(data[1]));
+    return MPU6050_FRAME_LEN;
+  }
+  return 1;
+}
+
+void SerialRead::ParseSingleFrame(const std::vector<uint8_t>& data,
+                                  TTLFrameType type) {
+  switch (type) {
+    case TTLFrameType::TIME:
+      break;
+    case TTLFrameType::ACCELERATION:
+      ParseAcceleration(data);
+      break;
+    case TTLFrameType::ANGULAR_VELOCITY:
+      ParseAngularVelocity(data);
+      break;
+    case TTLFrameType::ANGLE:
+      ParseAngle(data);
+      break;
+    default:
+      printf("ttl frame type not supported: %d\n", type);
+      break;
+  }
+}
+void SerialRead::ParseAcceleration(const std::vector<uint8_t>& data) {
+  uint8_t sum = 0;
+  for (size_t i = 0; i < MPU6050_FRAME_LEN - 1; i++) {
+    // printf("%02x\t", data[i]);
+    sum += data[i];
+  }
+
+  if (sum != data[10]) {
+    return;
+  }
+
+  // clang-format off
+  float Ax = ((int16_t)((int16_t)data[3] << 8 | data[2])) / 32768.0 * 16.0*9.8;  // m/s^2
+  float Ay = ((int16_t)((int16_t)data[5] << 8 | data[4])) / 32768.0 * 16.0*9.8;  // m/s^2
+  float Az = ((int16_t)((int16_t)data[7] << 8 | data[6])) / 32768.0 * 16.0*9.8;  // m/s^2
+  float temperature = ((int16_t)((int16_t)data[9] << 8 | data[8])) / 100.0;  // degree
+  // clang-format on
+
+  //   printf("sum: %02x, %02x\n", sum, data[10]);
+  printf("Ax: %f, Ay: %f, Az: %f, Temp: %f\n", Ax, Ay, Az, temperature);
+}
+void SerialRead::ParseAngularVelocity(const std::vector<uint8_t>& data) {
+  uint8_t sum = 0;
+  for (size_t i = 0; i < MPU6050_FRAME_LEN - 1; i++) {
+    sum += data[i];
+  }
+
+  if (sum != data[10]) {
+    return;
+  }
+
+  // clang-format off
+  float Wx = ((int16_t)((int16_t)data[3] << 8 | data[2])) / 32768.0 * 2000.0;  // degree/s
+  float Wy = ((int16_t)((int16_t)data[5] << 8 | data[4])) / 32768.0 * 2000.0;  // degree/s
+  float Wz = ((int16_t)((int16_t)data[7] << 8 | data[6])) / 32768.0 * 2000.0;  // degree/s
+  float temperature = ((int16_t)((int16_t)data[9] << 8 | data[8])) / 100.0;  // degree
+  // clang-format on
+
+  //   printf("sum: %02x, %02x\n", sum, data[10]);
+  printf("Wx: %f, Wy: %f, Wz: %f, Temp: %f\n", Wx, Wy, Wz, temperature);
+}
+void SerialRead::ParseAngle(const std::vector<uint8_t>& data) {
+  uint8_t sum = 0;
+  for (size_t i = 0; i < MPU6050_FRAME_LEN - 1; i++) {
+    sum += data[i];
+  }
+
+  if (sum != data[10]) {
+    return;
+  }
+
+  // clang-format off
+  float roll =  ((int16_t)((int16_t)data[3] << 8 | data[2])) / 32768.0 * 180.0;   // degree
+  float pitch = ((int16_t)((int16_t)data[5] << 8 | data[4])) / 32768.0 * 180.0;  // degree
+  float yaw =   ((int16_t)((int16_t)data[7] << 8 | data[6])) / 32768.0 * 180.0;    // degree
+  float temperature = ((int16_t)((int16_t)data[9] << 8 | data[8])) / 100.0;  // degree
+  // clang-format on
+
+  //   printf("sum: %02x, %02x\n", sum, data[10]);
+  printf("roll: %f, pitch: %f, yaw: %f, Temp: %f\n", roll, pitch, yaw,
+         temperature);
 }
 
 // main
