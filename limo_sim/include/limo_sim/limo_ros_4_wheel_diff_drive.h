1aa745f193011666cb54cbe6e72d90092e60f9fb
wangzheqie <wangzheqie@qq.com>
2021-08-13 18:16:41 +0800

    add gazebo utils

--- limo_sim/include/limo_sim/limo_ros_4_wheel_diff_drive.h
+++ limo_sim/include/limo_sim/limo_ros_4_wheel_diff_drive.h
@@ -1,12 +1,59 @@
 #ifndef WHEEL_4_DIFF_DRIVE_PLUGIN_H
 #define WHEEL_4_DIFF_DRIVE_PLUGIN
-#include <algorithm>
 #include <assert.h>
+#include <algorithm>
 
-#include <map>
+#include <gazebo/common/common.hh>
 #include <gazebo/physics/physics.hh>
+#include "gazebo_ros_utils.h"
+#include <map>
 
+#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 
-// reference: https://github.com/ros-simulation/gazebo_ros_pkgs/blob/noetic-devel/gazebo_plugins/include/gazebo_plugins/gazebo_ros_diff_drive.h
+#include <ros/advertise_options.h>
+#include <ros/callback_queue.h>
 
-#endif // WHEEL_4_DIFF_DRIVE_PLUG
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+
+namespace gazebo {
+class Joint;
+class Entity;
+
+class GazeboRosFourWheelDiffDrive : public ModelPlugin {
+  enum OdomSource {
+    ENCODER = 0,
+    WORLD = 1,
+  };
+
+ public:
+  GazeboRosFourWheelDiffDrive();
+  ~GazeboRosFourWheelDiffDrive();
+  void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
+  void Reset();
+
+ protected:
+  virtual void UpdateChild();
+  virtual void FiniChild();
+
+ private:
+  void publishOdometry(double step_time);
+  void getWheelVelocities();
+  void publishWheelTF();
+  void publishWheelJointState();
+  void UpdateOdometryEncoder();
+
+  GazeboRosPtr gazebo_ros_;
+};
+}  // namespace gazebo
+
+// reference:
+// https://github.com/ros-simulation/gazebo_ros_pkgs/blob/noetic-devel/gazebo_plugins/include/gazebo_plugins/gazebo_ros_diff_drive.h
+
+#endif  // WHEEL_4_DIFF_DRIVE_PLUG
