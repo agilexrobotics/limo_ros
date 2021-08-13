ad2092a037761f57f1800293a03c674952a790ee
wangzheqie <wangzheqie@qq.com>
2021-08-13 17:31:31 +0800

    add controller header file

---
+++ limo_sim/include/limo_sim/limo_ros_4_wheel_diff_drive.h
@@ -0,0 +1,12 @@
+#ifndef WHEEL_4_DIFF_DRIVE_PLUGIN_H
+#define WHEEL_4_DIFF_DRIVE_PLUGIN
+#include <algorithm>
+#include <assert.h>
+
+#include <map>
+#include <gazebo/physics/physics.hh>
+
+
+// reference: https://github.com/ros-simulation/gazebo_ros_pkgs/blob/noetic-devel/gazebo_plugins/include/gazebo_plugins/gazebo_ros_diff_drive.h
+
+#endif // WHEEL_4_DIFF_DRIVE_PLUG
