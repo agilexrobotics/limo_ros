65f4bda0c63f3e2d1646f675ed45a36c76e64cbe
agilexrobotics <support@agilex.ai>
2021-09-07 17:16:31 +0800

    remove opencv imshow

--- limo_bringup/scripts/detect_traffic_lights.py
+++ limo_bringup/scripts/detect_traffic_lights.py
@@ -10,6 +10,9 @@ from read_realsense_image import ReadImage
 
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Int16
+
+pub = None
 
 
 def DetectColor(image):
@@ -52,23 +55,26 @@ def DetectColor(image):
         return 0
 
 
-class TLState(Enum): # traffic light state
+class TLState(Enum):  # traffic light state
     red = 1
     yellow = 2
     green = 3
 
-class TLType(Enum): # traffic light type
+
+class TLType(Enum):  # traffic light type
     regular = 0
     five_lights = 1
     four_lights = 2
 
-def ImageResize(image, height, inter= cv2.INTER_AREA):
+
+def ImageResize(image, height, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
     r = height / float(h)
     dim = (int(w*r), height)
-    resized = cv2.resize(image, dim, interpolation = inter)
+    resized = cv2.resize(image, dim, interpolation=inter)
     return resized
+
 
 def DetectState(image, type):
     image = ImageResize(image, 200)
@@ -77,21 +83,23 @@ def DetectState(image, type):
     output = image.copy()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("gray", gray)
+    # cv2.imshow("gray", gray)
 
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=30)
+    circles = cv2.HoughCircles(
+        gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=30)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        a,b,c = circles.shape
-        print(a,b,c)
+        a, b, c = circles.shape
+        print(a, b, c)
         for i in range(b):
-            cv2.circle(cimage, (circles[0][i][0], circles[0][i][1]), circles[0][i][2], (0, 0, 255), 3, cv2.LINE_AA)
-            cv2.circle(cimage, (circles[0][i][0], circles[0][i][1]), 2, (0, 255, 0), 3,  
-                    cv2.LINE_AA)  # draw center of circle
+            cv2.circle(cimage, (circles[0][i][0], circles[0][i][1]),
+                       circles[0][i][2], (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.circle(cimage, (circles[0][i][0], circles[0][i][1]), 2, (0, 255, 0), 3,
+                       cv2.LINE_AA)  # draw center of circle
 
-    cv2.imshow("circle_img", cimage)
-    cv2.waitKey(0)
+    # cv2.imshow("circle_img", cimage)
+    # cv2.waitKey(0)
 
     overallState = 0
     stateArrow = 0
@@ -99,77 +107,94 @@ def DetectState(image, type):
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
+        for i in circles[0, :]:
             if i[1] < i[2]:
                 i[1] = i[2]
-            
+
             roi = image[(i[1]-i[2]):(i[1]+i[2]), (i[0]-i[2]):(i[0]+i[2])]
 
             color = DetectColor(roi)
             if color > 0:
-                stateSolid= color
-        
+                stateSolid = color
+
         if type == 1:
-            overallState = stateArrow + stateSolid +1
+            overallState = stateArrow + stateSolid + 1
         elif type == 2:
-            overallState = stateArrow +7
+            overallState = stateArrow + 7
         else:
             overallState = stateSolid
-    
+
     return overallState
 
+
 def PlotLightResult(images):
-    for i , image in enumerate(images):
+    for i, image_path in enumerate(images):
         plt.subplot(1, len(images), i+1)
-        img = mpimg.imread(image)
-        label = TLState(DetectState(cv2.imread(image), TLType.regular.value)).name
+        image = cv2.imread(image_path)
+        image_roi = image[0:260, 400:640]
+        # img = mpimg.imread(image_roi)
+        label = TLState(DetectState(image_roi, TLType.regular.value)).name
         plt.title(label)
-        plt.imshow(img)
+        plt.imshow(image_roi)
     plt.show()
 
+
 def PlotRealsenseResult(image):
-    image_roi = image[0:200, 300:380]
-    cv2.imshow("img", image_roi)
+    # image_roi = image[0:200, 300:380]
+    image_roi = image[0:260, 400:640]
+
+    # cv2.imshow("img", image_roi)
 
     img = image_roi
     label = TLState(DetectState(image_roi, TLType.regular.value)).name
-    plt.plot()
-    plt.title(label)
-    plt.imshow(img)
-    plt.show()
-    cv2.waitKey(0)
+    # plt.plot()
+    # plt.title(label)
+    # plt.imshow(img)
+    # plt.show()
+    print(label)
+    # cv2.waitKey(0)
+
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I get : %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + "I get traffic light: %s", data.data)
     ri = ReadImage()
     image = ri.read()
     if image is not None:
-        PlotRealsenseResult(image)
+        color_idx = PlotRealsenseResult(image)
+
+        if color_idx == 0 or color_idx == 1:  # 0 red, 1 green
+            pub.publish(Int16(color_idx))
     else:
         print("can not read realsense image")
 
+
 def ROSSubscribe():
     rospy.init_node('traffic_light_detecter')
-    rospy.Subscriber("detect_light", String , callback)
+    rospy.Subscriber("traffic_light_status", Int16, callback)
+
+    global pub
+    pub = rospy.Publisher("traffic_light_status", Int16, queue_size=1)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rate.sleep()
+
 
 if __name__ == "__main__":
 
     # #Test 1.  use local image
     # light_img_path = ["images/red.jpg", "images/yellow.png", "images/green.png"]
     # light_img_path = ["traffic_light_red_roi.png", "traffic_light_yellow_roi.png", "traffic_light_green_roi.png"]
+    # light_img_path = ["red_Color.png"]
     # random.shuffle(light_img_path)
     # PlotLightResult(light_img_path)
 
-    #Test 2.  use realsense capture image
-    ri = ReadImage()
-    image = ri.read()
-    if image is not None:
-        PlotRealsenseResult(image)
-    else:
-        print("can not read realsense image")
+    # #Test 2.  use realsense capture image
+    # ri = ReadImage()
+    # image = ri.read()
+    # if image is not None:
+    #     PlotRealsenseResult(image)
+    # else:
+    #     print("can not read realsense image")
 
-    # ROSSubscribe()
+    ROSSubscribe()
